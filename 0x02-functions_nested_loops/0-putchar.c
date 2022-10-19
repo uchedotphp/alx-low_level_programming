adabb4070e3e8e94c3ea06f9cd2537814e23c9b4
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2022-10-18 21:26:13 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,24 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - prints Putchar
  *
- * Return: Always returns 0
+ * Description: prints _putchar
  *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
